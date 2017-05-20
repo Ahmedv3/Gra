484de8624b7fcedfbf1c59ee47615cbc1699c7c1
Jakub Czarnecki <ahmed122.90@o2.pl>
2017-05-20 16:32:00 +0200

    Add files via upload
    Przerzucenie kodu do Klasu "plansza", dodanie metodu losuj oraz komunikatu o ilości wyrzuconych oczek.

--- plansza.py
+++ plansza.py
@@ -1,19 +1,36 @@
-import pygame, sys
+import pygame, sys, random
 
-screen = pygame.display.set_mode((800, 600))
+class plansza(object):
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit(0)
+    def __init__(self):
+        # Konfiguracja
+        self.screen =  pygame.display.set_mode((800, 600))
+        self.size = self.screen.get_size()
+        pygame.init()
+        while True:
+            for self.event in pygame.event.get():
+                if self.event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif self.event.type == pygame.KEYDOWN and self.event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+                if self.event.type == pygame.KEYDOWN and self.event.key == pygame.K_l:
+                    self.losowanie()
+                    print("Wyrzuciłeś:", self.oczko, "oczek.")
+            self.rysuj()
+            self.screen.fill((0, 0, 0))
 
-    size = screen.get_size()
-    kwadrat = pygame.Rect(size[0]/2, size[1]/2, 40, 40)
-    kwadrat2 = pygame.Rect(size[0]/2, size[1]/2-41, 40, 40)
-    kwadrat3 = pygame.Rect(size[0]/2, size[1]/2-82, 40, 40)
-    pygame.draw.rect(screen, (0, 150, 255), kwadrat)
-    pygame.draw.rect(screen, (0, 150, 255), kwadrat2)
-    pygame.draw.rect(screen, (0, 150, 255), kwadrat3)
-    pygame.display.flip()
+    def rysuj(self):
+
+        self.kwadrat = pygame.Rect(self.size[0] / 2, self.size[1] / 2, 40, 40)
+        self.kwadrat2 = pygame.Rect(self.size[0] / 2, self.size[1] / 2 - 41, 40, 40)
+        self.kwadrat3 = pygame.Rect(self.size[0] / 2, self.size[1] / 2 - 82, 40, 40)
+        pygame.draw.rect(self.screen, (0, 150, 255), self.kwadrat)
+        pygame.draw.rect(self.screen, (0, 150, 255), self.kwadrat2)
+        pygame.draw.rect(self.screen, (0, 150, 255), self.kwadrat3)
+        pygame.display.flip()
+
+    def losowanie(self):
+        self.oczko = random.randint(1, 6)
+
+plansza()
+

bdfc906b51002574be8ab3264d73c9494c86b840
Jakub Czarnecki <ahmed122.90@o2.pl>
2017-05-24 21:34:16 +0200

    Add files via upload
    Wyświetlanie ilości oczek w oknie.

--- plansza.py
+++ plansza.py
@@ -14,7 +14,13 @@ class Plansza(object):
         self.clock = pygame.time.Clock()
         self.wspolrzedne_srodkow = []
         self.ruch = 0
+
+
+        # kolory
         self.zielony = (0, 250, 0)
+        self.niebieski = (0, 150, 255)
+        self.czarny = (0, 0, 0)
+
         pygame.init()
 
         while True:
@@ -40,24 +46,35 @@ class Plansza(object):
             self.obiekt = Vector2(self.wsp_x, self.wsp_y)
             self.wspolrzedne_srodkow.append(self.obiekt)
             self.kwadrat = pygame.Rect(self.wsp_x, self.wsp_y, 50, 50)
-            pygame.draw.rect(self.screen, (0, 150, 255), self.kwadrat)
+            pygame.draw.rect(self.screen, self.niebieski, self.kwadrat)
         for j in range(0, 275, 55):
             self.wsp_x = 640
             self.wsp_y = 300 - j
             self.obiekt = Vector2(self.wsp_x, self.wsp_y)
             self.wspolrzedne_srodkow.append(self.obiekt)
             self.kwadrat = pygame.Rect(self.wsp_x, self.wsp_y, 50, 50)
-            pygame.draw.rect(self.screen, (0, 150, 255), self.kwadrat)
+            pygame.draw.rect(self.screen, self.niebieski, self.kwadrat)
         print(self.wspolrzedne_srodkow)
         pygame.display.flip()
 
     def random(self):
         self.los = random.randint(1, 6)
+        self.text = str(self.los)
+        self.text2 = "Twoja liczba to: "
+        self.font = pygame.font.SysFont("dejavusans", 20)
+        self.txt_rendering = self.font.render(self.text, 1, (250, 250, 250))
+        self.txt_rendering2 = self.font.render(self.text2, 1, (250, 250, 250))
+        self.zaslona = pygame.Rect(30, 550, 40, 40)
+        pygame.draw.rect(self.screen, self.czarny, self.zaslona)
+        self.zaslona_napis = pygame.Rect(60, 550, 165, 40)
+        pygame.draw.rect(self.screen, self.czarny, self.zaslona_napis)
+        self.screen.blit(self.txt_rendering2, (30, 550))
+        self.screen.blit(self.txt_rendering, (190, 550))
         print(self.los)
 
     def move(self):
 
-        self.tempRuch = self.ruch+self.los
+        self.tempRuch = self.ruch + self.los
         if self.tempRuch >= len(self.wspolrzedne_srodkow):
             self.tak = pygame.Rect(640, 80, 50, 50)
             self.pole1 = pygame.draw.rect(self.screen, self.zielony, self.tak)
@@ -68,8 +85,8 @@ class Plansza(object):
 
         else:
             self.pole = self.wspolrzedne_srodkow[self.tempRuch]
-            self.tak = pygame.Rect(self.pole.x, self.pole.y, 50, 50)
-            pygame.draw.rect(self.screen, self.zielony, self.tak)
+            self.figura = pygame.Rect(self.pole.x, self.pole.y, 50, 50)
+            pygame.draw.rect(self.screen, self.zielony, self.figura)
             pygame.display.flip()
             self.ruch = self.tempRuch
             print("krok")
@@ -82,7 +99,6 @@ class Pawn(object):
         pass
 
     def position_draw(self):
-
         pass
 
 
